0a678df76729326480e20b43f497bf745a49f810
Deanna Wright <deanna.j.wright@gmail.com>
2023-02-04 17:46:47 +0100

    deployed to heroku

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :celebrity
+
+  validates :starts_on, :ends_on, presence: true
+
+  # must add validation for start and ends dates
+  # make sure chosen dates are not taken by other booking
+
+  # maybe validates uniqueness of, scope: thing
 end
--- app/models/celebrity.rb
+++ app/models/celebrity.rb
@@ -1,3 +1,8 @@
 class Celebrity < ApplicationRecord
   belongs_to :user
+
+  # add photo { has_many_attached ? :photo} w cloudinary
+  # validate :photo presence true
+
+  # must add add celebrity user story validations
 end
