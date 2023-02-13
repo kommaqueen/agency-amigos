c52632d816a2a744b8b1bd00a96a0545a1c6170d
Caio Zimbardi <cbzimbardi@gmail.com>
2023-02-13 22:52:26 +0100

    styles and destroy method for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,12 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to celebrity_path, status: :see_other
+  end
+
   private
 
   def set_celebrity
